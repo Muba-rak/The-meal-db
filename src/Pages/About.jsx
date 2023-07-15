a178b2805586c177e2aa5be85cec87c6b0b6a42f
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-07-15 11:18:18 +0100

    refactored styles

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,14 +2,38 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container-md about-section">
-      <h2 className="text-success fw-bold">About Us</h2>
-      <p className="mt-1 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illo
-        debitis molestiae nostrum vero necessitatibus cum quam nihil aliquid
-        consectetur adipisci quaerat, ratione iusto voluptates perferendis
-        nesciunt, labore rem fugit aperiam fugiat fuga aspernatur, laudantium
-        ea. Totam error quas nulla?
+    <div className="container-md">
+      <h2 className="text-success fw-light">About Us</h2>
+      <p className="my-3  lh-lg">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
+        sunt officia, adipisci debitis, hic repudiandae quo et at incidunt ad
+        saepe consequatur rerum cupiditate, ea molestiae praesentium ullam
+        ducimus. Dolores.
+      </p>
+      <h2 className="text-success fw-light">What we Do?</h2>
+      <p className="my-3  lh-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+        temporibus architecto ducimus ipsa dolorum delectus fuga vitae modi
+        repudiandae inventore, eveniet perferendis tempora non. In perspiciatis
+        ad neque numquam necessitatibus!
+      </p>
+      <h2 className="text-success fw-light">Our Mission</h2>
+      <p className="my-3  lh-lg">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam magnam
+        voluptates quod ea culpa laboriosam recusandae adipisci! Iure maxime,
+        iste debitis error enim ab delectus quibusdam possimus eligendi sint?
+        Quasi, harum tempore possimus suscipit fugiat tenetur necessitatibus
+        quia debitis hic?
+      </p>
+      <h2 className="text-success fw-light">Our Vision</h2>
+      <p className="my-3 lh-lg">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+        perferendis deleniti a excepturi! Ea fugit voluptates totam nesciunt,
+        reiciendis officiis doloremque magnam eos dolor laborum sunt harum
+        libero hic doloribus explicabo fugiat temporibus iste voluptas! Quis
+        fuga perferendis officiis, a blanditiis dolore atque quaerat dolores
+        veritatis, quibusdam id eum obcaecati inventore nihil. Aut perspiciatis
+        quis, asperiores quaerat sed hic nemo!
       </p>
     </div>
   );
--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,10 +3,16 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className="container-md about-section text-center">
-      <h1>oops, Page not found</h1>
-      <Link to="/">
-        <button className="btn btn-success">Go back to Homepage</button>
+    <div className="container-md  text-center my-5 my-md-1">
+      <img
+        src="https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1900.jpg?size=626&ext=jpg&ga=GA1.2.2085038139.1673344453&semt=ais"
+        alt="error"
+        className="img-fluid d-block mx-auto"
+      />
+      <Link to="/" className="text-decoration-none">
+        <button className="btn btn-success d-block my-1 mx-auto">
+          Go Back to Homepage
+        </button>
       </Link>
     </div>
   );
